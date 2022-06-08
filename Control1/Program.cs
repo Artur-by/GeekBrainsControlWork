3b8f655fa809647f8fd110d9ea2179b582b39dec
Artur-by <artur.by@mail.ru>
2022-06-08 20:34:14 +0300

    Add methods for creating an empty array and output to the console

--- Control1/Program.cs
+++ Control1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив строк, 
+// элементы которого меньше либо равны 3 символам
+
+
+
+// метод создания массива
+string[] CreateArray(int N)
+{
+    string[] array = new string[N];
+
+    return array;
+}
+
+//метод вывода элементов массива в консоль
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($" {array[i]} ");
+}
+
+Console.Write("Введите длину массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string[] array = CreateArray(N);
+for (int i = 0; i < N; i++)
+{
+    Console.Write($"Введите {i + 1}-й элемент массива: ");
+    array[i] = Convert.ToString(Console.ReadLine());
+}
+
+PrintArray(array);
